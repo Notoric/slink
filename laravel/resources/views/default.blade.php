d9a5ab53e547cd7eab3fa696df644196e676d91a
Notoric <tom@acornes.com>
2024-06-10 23:07:48 +0100

    Fixed links to use absolute path

--- laravel/resources/views/default.blade.php
+++ laravel/resources/views/default.blade.php
@@ -12,13 +12,13 @@
                 <a id="logo" href="/home">[slink]</a>
                 @if (Auth::check())
                     <nav>
-                        <a href="profile"><img title="Profile" src="{{ asset('img/icons/profile.svg') }}"></a>
-                        <a href="logout"><img title="Logout" src="{{ asset('img/icons/logout.svg') }}"></a>
+                        <a href="/profile"><img title="Profile" src="{{ asset('img/icons/profile.svg') }}"></a>
+                        <a href="/logout"><img title="Logout" src="{{ asset('img/icons/logout.svg') }}"></a>
                     </nav>
                 @else
                     <nav>
-                        <a href="register">Register</a>
-                        <a href="login">Log In</a>
+                        <a href="/register">Register</a>
+                        <a href="/login">Log In</a>
                     </nav>
                 @endif
             </div>
